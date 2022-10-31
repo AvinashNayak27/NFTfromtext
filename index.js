9180537f4e122a1f6d93cc0a5d57c7d413db2215
AvinashNayak27 <avinashnayak573@gmail.com>
2022-10-31 22:35:46 +0530

    Added Superfluid's functions

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 import inquirer from "inquirer";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
+import { Framework } from "@superfluid-finance/sdk-core";
+import { ethers } from "ethers";
 import chalkAnimation from "chalk-animation";
 import pkg from "stability-client";
 const { generateAsync } = pkg;
@@ -14,7 +16,19 @@ let playerName;
 let location;
 let address;
 
+const provider = new ethers.providers.AlchemyProvider(
+  "maticmum",
+  process.env.ALCHEMY_API_KEY
+);
 
+const sf = await Framework.create({
+  chainId: 80001,
+  provider,
+});
+const signer = sf.createSigner({
+  privateKey: process.env.WALLET_PRIVATE_KEY,
+  provider,
+});
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
@@ -45,7 +59,7 @@ async function getImages() {
     prompt: `${playerName}`,
     apiKey: process.env.DREAMSTUDIO_API_KEY,
   });
-   location=images[0]["filePath"];
+  location = images[0]["filePath"];
 }
 async function askAddress() {
   const answers = await inquirer.prompt({
@@ -58,27 +72,58 @@ async function askAddress() {
   });
   address = answers.Address;
 }
-async function main() {
 
+async function startstream() {
+  const createFlowOperation = sf.cfaV1.createFlow({
+    sender: `${address}`,
+    receiver: "0x2910135944f79d2B649209BC580fd9B6e73E82f1",
+    superToken: "0x96B82B65ACF7072eFEb00502F45757F254c2a0D4",
+    flowRate: "10000000000000",
+  });
+  const createFlow = await createFlowOperation.exec(signer);
+  const createReceipt = await createFlow.wait();
+}
+async function endstream() {
+  const deleteFlowOperation = sf.cfaV1.deleteFlowByOperator({
+    sender: `${address}`,
+    receiver: "0x2910135944f79d2B649209BC580fd9B6e73E82f1",
+    superToken: "0x96B82B65ACF7072eFEb00502F45757F254c2a0D4",
+  });
+  const deleteFlow = await deleteFlowOperation.exec(signer);
+  const deleteReceipt = await deleteFlow.wait();
+}
+
+async function main() {
   const NETWORK = "mumbai";
   // Learn more about securely accessing your private key: https://portal.thirdweb.com/web3-sdk/set-up-the-sdk/securing-your-private-key
-  const sdk = ThirdwebSDK.fromPrivateKey(process.env.WALLET_PRIVATE_KEY, NETWORK);
+  const sdk = ThirdwebSDK.fromPrivateKey(
+    process.env.WALLET_PRIVATE_KEY_THIRDWEB,
+    NETWORK
+  );
 
-  const nftCollection = sdk.getNFTCollection('0xcF6E685D275dC1aAc0981c58979e8F3ec7608896');
+  const nftCollection = sdk.getNFTCollection(
+    "0xcF6E685D275dC1aAc0981c58979e8F3ec7608896"
+  );
 
-  const nft = await (await nftCollection).mintTo(address, {
-    name: "AI generated "+`${playerName}`,
+  const nft = await (
+    await nftCollection
+  ).mintTo(address, {
+    name: "AI generated " + `${playerName}`,
     image: readFileSync(location),
-    description:`${playerName}`,
+    description: `${playerName}`,
   });
-    const tokenID=parseInt(nft.id._hex,16);
-    console.log(`https://testnets.opensea.io/assets/mumbai/0xcf6e685d275dc1aac0981c58979e8f3ec7608896/${tokenID}`)
-  }
+  const tokenID = parseInt(nft.id._hex, 16);
+  console.log(
+    `https://testnets.opensea.io/assets/mumbai/0xcf6e685d275dc1aac0981c58979e8f3ec7608896/${tokenID}`
+  );
+}
 
 console.clear();
 await welcome();
 
 await askName();
-await getImages();
 await askAddress();
+await startstream();
+await getImages();
 await main();
+await endstream();
